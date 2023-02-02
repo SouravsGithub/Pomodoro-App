fd501cea8d516a892900e37f7e6c36a7b579735a
SouravsGithub <105702695+SouravsGithub@users.noreply.github.com>
2023-02-02 22:12:35 +0530

    added the timer feature along with start and reset feature

---
+++ script.js
@@ -0,0 +1,60 @@
+// reference to the elements
+const minHolder = document.querySelector(".minutes");
+const secHolder = document.querySelector(".seconds");
+const startBtn = document.querySelector(".start");
+const resetBtn = document.querySelector(".reset");
+
+// initial display
+let minutes = 25;
+let seconds = "00";
+
+if (minutes >= 0 && minutes < 10) {
+    minHolder.innerHTML = "0" + minutes;
+} else {
+    minHolder.innerHTML = minutes;
+}
+secHolder.innerHTML = seconds;
+
+let myIntervalID;
+startBtn.addEventListener("click", () => {
+    // start timer
+    seconds = 0;
+    myIntervalID = setInterval(countdown, 1000);
+});
+resetBtn.addEventListener("click", () => {
+    // reset timer
+    clearInterval(myIntervalID);
+    minutes = 25;
+    seconds = "00";
+    if (minutes >= 0 && minutes < 10) {
+        minHolder.innerHTML = "0" + minutes;
+    } else {
+        minHolder.innerHTML = minutes;
+    }
+    secHolder.innerHTML = seconds;
+})
+function countdown() {
+    // if the timer is on 00:00 then stop
+    if (minutes === 0 && seconds === 0) {
+        clearInterval(myIntervalID);
+    } else {
+        // otherwise run the timer
+        if (seconds !== 0) {
+            seconds--;
+        } else {
+            minutes = minutes - 1;
+            seconds = 59;
+        }
+        if (minutes >= 0 && minutes < 10) {
+            minHolder.innerHTML = "0" + minutes;
+        } else {
+            minHolder.innerHTML = minutes;
+        }
+        if (seconds >= 00 && seconds < 10) {
+            secHolder.innerHTML = "0" + seconds;
+        } else {
+            secHolder.innerHTML = seconds;
+        }
+    }
+
+}
